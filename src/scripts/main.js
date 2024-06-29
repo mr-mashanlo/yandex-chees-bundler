2c2a1b5b2d3d4b3eeaacb6ea307bef3f18a67fdc
mr-mashanlo <mr.mashanlo@yandex.ru>
2024-06-29 12:38:50 +0500

    add member block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,9 @@
+import Slider from './slider.js';
 import ticker from './ticker.js';
 
 document.addEventListener( 'DOMContentLoaded', () => {
-  
+
   ticker( { element: '.ticker' } );
-  
+  new Slider( { element: '.slider', slides: 3 } );
+
 } );
---
+++ src/scripts/slider.js
@@ -0,0 +1,88 @@
+class Slider {
+
+  constructor( options ) {
+    this.element = document.querySelector( options.element );
+    this.slider = this.element.querySelector( '.slider-list' );
+    this.sliderSlides = this.slider.querySelectorAll( '.slider-list__item' );
+    this.controls = this.element.querySelector( '.slider-control' );
+    this.slides = options.slides;
+    this.position = options.slides;
+    this.shift = 0;
+
+    this.init();
+  }
+
+  init = () => {
+    this.setSlidesWidth();
+    this.handleControlsClick();
+    this.displayPosition();
+    this.displayTotalSlides();
+    this.updateButtonStatus();
+  };
+
+  setSlidesWidth = () => {
+    this.sliderSlides.forEach( slide => {
+      slide.style.width = `${100 / this.slides}%`;
+    } );
+  };
+
+  updateButtonStatus = () => {
+    const prev = this.controls.querySelector( '.slider-control__prev' );
+    const next = this.controls.querySelector( '.slider-control__next' );
+
+    if ( this.position <= this.slides ) {
+      prev.disabled = true;
+    } else if ( this.position >= this.getTotalSlides() ) {
+      next.disabled = true;
+    } else {
+      prev.disabled = false;
+      next.disabled = false;
+    }
+  };
+
+  handleControlsClick = () => {
+    this.controls.addEventListener( 'click', event => { 
+      const button = event.target.closest( 'button' );
+      if ( !button ) return; 
+
+      if ( button.classList.contains( 'slider-control__prev' ) ) {
+        this.position = this.position - 1;
+        this.shift = this.shift - this.getSlideWidth();
+      } else if ( button.classList.contains( 'slider-control__next' ) ) {
+        this.position = this.position + 1;
+        this.shift = this.shift + this.getSlideWidth();
+      }
+
+      this.updateButtonStatus();
+      this.displayPosition();
+      this.moveSlides();
+    } );
+  };
+
+  moveSlides = () => {
+    this.sliderSlides.forEach( item => {
+      item.style.transform = `translateX( -${this.shift}px )`;
+    } );
+  };
+
+  getSlideWidth = () => {
+    return this.sliderSlides[0].offsetWidth;
+  };
+
+  getTotalSlides = () => {
+    return this.sliderSlides.length;
+  };
+  
+  displayPosition = () => {
+    const position = this.controls.querySelector( '.slider-counter__position' );
+    position.textContent = this.position;
+  };
+
+  displayTotalSlides = () => {
+    const totalSlides = this.controls.querySelector( '.slider-counter__total-slides' );
+    totalSlides.textContent = this.getTotalSlides();
+  };
+
+}
+
+export default Slider;
